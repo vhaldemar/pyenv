ae2fa46b0e19c22378f3165ed5a5e9d2cb11033a
alvinmax <alvinmax@yandex-team.ru>
2020-06-10 11:49:14 +0300

    tiny refactoring

--- pyenv/serialization.py
+++ pyenv/serialization.py
@@ -1,10 +1,8 @@
+import copy
 from abc import abstractmethod
 from pickle import Pickler, Unpickler
-from typing import BinaryIO, Iterable, Dict
-
-"""
-serialization: extending pickler/unpickler
-"""
+from typing import BinaryIO, Iterable, Dict, Set
+from .utils import StreamingUtils
 
 
 class CustomPickler(Pickler):
@@ -16,9 +14,23 @@ class CustomUnpickler(Unpickler):
 
 
 class PicklerResult:
-    value: BinaryIO
-    clusters: Iterable[Iterable[str]]
-    non_serialized_vars: Iterable[str]
+    def __init__(self, value: BinaryIO, clusters: Iterable[Set[str]], non_serialized_vars: Set[str]):
+        self._value = value
+        self._clusters = copy.deepcopy(clusters)
+        self._non_serialized_var = set(non_serialized_vars)
+        self._processed = False
+
+    def clusters(self) -> Iterable[Set[str]]:
+        return copy.deepcopy(self._clusters)
+
+    def non_non_serialized_vars(self) -> Set[str]:
+        return set(self._non_serialized_var)
+
+    def transfer(self, output: BinaryIO) -> None:
+        if self._processed:
+            raise IOError('Data has been already processed')
+        self._processed = True
+        StreamingUtils.transfer(self._value, output)
 
 
 class PicklerAgent:
