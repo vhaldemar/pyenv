b2763903611a007ccaf527e2fe7b85815be1c6a6
drykovanov <drykovanov@yandex-team.ru>
2020-07-10 13:24:42 +0300

    starting pickling

--- src/main/python/ipystate/serialization.py
+++ src/main/python/ipystate/serialization.py
@@ -1,5 +1,9 @@
+import sys
+
 from abc import abstractmethod
 from typing import BinaryIO, Iterable, Dict, Set, Tuple, Any
+
+from pickle import Pickler, Unpickler
 
 from .impl.components_fuser import ComponentsFuser
 from .impl.walker import Walker
@@ -52,7 +56,8 @@ class LoadedComponent:
 
 class Serializer:
 
-    def __init__(self):
+    def __init__(self, pickler: Pickler):
+        self._pickler = pickler
         self._walker = Walker()
 
     @abstractmethod
@@ -73,13 +78,53 @@ class Serializer:
         affected_var_names = ComponentsFuser.fuse(touched_names, all_components)
         return affected_var_names, all_components
 
+    @abstractmethod
+    def _primitive_var_payload(self, name: str, value: Any) -> BinaryIO:
+        pass
+
+    def _start_var_pickling(self, name: str, value: Any) -> Pickler:
+        return self._pickler
+
+    def _on_var_pickled(self, pickler: Pickler, name: str, value: Any):
+        pass
+
+    def _on_var_pickle_error(self, pickler: Pickler, name: str, value: Any, ex: Exception):
+        pass
+
+    def _on_var_pickle_finally(self, pickler: Pickler, name: str, value: Any):
+        pass
+
+    # @abstractmethod
+    # def _component_payload(self, name, value):
+    #     pass
+
+    def _dump_component_pickle(self, component: Set[str], all_variables: Dict[str, object]) -> Dump:
+        var_names = set()
+        payload = bytearray()
+        non_serialized_vars = set()
+
+        comp_sorted_vars = sorted(component)
+        for var_name in comp_sorted_vars:
+            var_value = all_variables.get(var_name)
+            pickler = self._start_var_pickling(var_name, var_value)
+            try:
+                self._on_var_pickled(pickler, var_name, var_value)
+            except Exception as e:
+                self._on_var_pickle_error(pickler, var_name, var_value, e)
+            finally:
+                self._on_var_pickle_finally(pickler, var_name, var_value)
+
+        # TODO pickle
+        return ComponentDump(var_names=var_names, payload=payload, non_serialized_vars=non_serialized_vars)
+
     def _dump_component(self, component: Set[str], all_variables: Dict[str, object]) -> Dump:
         if len(component) == 1 and self._is_primitive(all_variables.get(list(component)[0])):
-            # TODO payload
-            return PrimitiveDump(name=list(component)[0], payload=None)
+            name = list(component)[0]
+            value = all_variables.get(name)
+            payload = self._primitive_var_payload(name, value)
+            return PrimitiveDump(name=name, payload=payload)
         else:
-            # TODO payload and non_serialized_vars
-            return ComponentDump(var_names=component, payload=None, non_serialized_vars=set())
+            return self._dump_component_pickle(component, all_variables)
 
     def dump(self, variables: Dict[str, object], dirty: Iterable[str]) -> Tuple[Iterable[ComponentStruct], Iterable[Dump]]:
         affected_var_names, components = self._compute_affected(variables, dirty)
