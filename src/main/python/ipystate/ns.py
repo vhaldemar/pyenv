24c9533efbeec4b2f7b01930fca812c44ba012cc
drykovanov <drykovanov@yandex-team.ru>
2020-11-03 12:30:50 +0300

    switching Hasher to ChangeDetector in ns

--- src/main/python/ipystate/ns.py
+++ src/main/python/ipystate/ns.py
@@ -5,7 +5,7 @@ from typing import Iterable, Dict, Set, Tuple
 from ipystate.serialization import Serializer, Deserializer, PrimitiveDump, ComponentDump
 from ipystate.change import AtomicChange, PrimitiveAtomicChange, ComponentAtomicChange, RemoveAtomicChange
 from ipystate.impl.walker import Walker
-from ipystate.hasher import HasherImpl, Hasher
+from ipystate.impl.changedetector import ChangeDetector, ChangeStage, ChangedState, DummyChangeDetector
 
 class Namespace(dict):
     def __init__(self, init: Dict[str, object], serializer: Serializer, deserializer: Deserializer):
@@ -18,7 +18,7 @@ class Namespace(dict):
         self._deserializer = deserializer
         self._walker = Walker(dispatch_table=serializer.configurable_dispatch_table)
         self._reset(new_comps=None)
-        self._hasher: Hasher = HasherImpl()
+        self._change_detector = DummyChangeDetector()
 
     def _on_reset(self):
         """
@@ -62,10 +62,10 @@ class Namespace(dict):
         if name in self._deleted:
             return True
         value = super().__getitem__(name)
-        changed = self._hasher.has_changed(name, value)
-        self._hasher.update_hash(name, value)
-        # print(f"{name}, changed={changed}")
-        return changed
+
+        change_state = self._change_detector.update(ChangeStage.RAW, name, value)
+
+        return False if (ChangedState.UNCHANGED == change_state) else True
 
     def _compute_comps(self) -> Iterable[Set[str]]:
         return self._walker.walk(
