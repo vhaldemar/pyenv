9e1675c8e0cef69dc8ff71a28303925c449e3c4b
Nikita Sokolov <faucct@yandex-team.ru>
2021-07-16 09:27:51 +0300

    fixup! fix recursion problem

--- src/main/python/ipystate/impl/dispatch/dispatcher.py
+++ src/main/python/ipystate/impl/dispatch/dispatcher.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 class Dispatcher:
     @staticmethod
     def _reduce_without_args(_type):
-        return lambda _: _type, ()
+        return lambda _: (_type, ())
 
     @abstractmethod
     def register(self, dispatch):
