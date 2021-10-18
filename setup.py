91baa7b3a69c596a7ec018fe26a35b8e30c78758
Nikita Sokolov <faucct@yandex-team.ru>
2021-10-18 14:45:34 +0300

    CLOUDSUPPORT-100820: Pyarrow should not segfault

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     install_requires=[
         'ipython>=7.13.0,<=7.19.0',
         'cloudpickle@git+https://github.com/faucct/cloudpickle.git@feature/persistent-function-globals#egg=cloudpickle',
-        'pyarrow>=0.17.1,<=3.0.0',
+        'pyarrow>=0.17.1,<=5.0.0',
         'pybase64>=1.0.0,<=1.0.2',
         'cython==0.29.5',
         'pympler==0.9',
